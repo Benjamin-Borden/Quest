72d4cd247decab48715f806f1a20429710197701
Benjamin Borden <benborden@Benjamins-MacBook-Air.local>
2020-04-12 15:27:54 -0500

    added a way to generate a proper board state in legendsboard

--- LegendsBoard.java
+++ LegendsBoard.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class LegendsBoard extends Board{
@@ -7,15 +8,22 @@ public class LegendsBoard extends Board{
 
     public LegendsBoard(Game g) {
         boardState = new Tile[8][8];
-        for(int i=0;i<8;i++){
-            if(i==0||i==7){
-                boardState[i]= new Tile[]{new BlockingTile(),new BlockingTile(),new BlockingTile(),new BlockingTile(),new BlockingTile(),new BlockingTile(),new BlockingTile(),new BlockingTile()};
-            }else{
-                boardState[i]= new Tile[]{new BlockingTile(),new BlockingTile(),new BlockingTile(),new BlockingTile(),new BlockingTile(),new BlockingTile(),new BlockingTile(),new BlockingTile()};
-            }
-        }
+        game = g;
+        generateBoardState();
     }
+    private void generateBoardState(){
+        Object[] temp = Input.flatten(new Item[][]{game.getPotions(),game.getArmor(),game.getWeapons(),game.getSpells()}).toArray();
+        Item[] items = Arrays.copyOf(temp,temp.length,Item[].class);
+        boardState[0] = new Tile[]{new Market(items),new Market(items),new BlockingTile(),new Market(items),new Market(items),new BlockingTile(),new Market(items),new Market(items)};
+        boardState[1] = new Tile[]{new PlainTile(),new PlainTile(),new BlockingTile(),new CaveTile(),new PlainTile(),new BlockingTile(),new BushTile(),new BushTile()};
+        boardState[2] = new Tile[]{new PlainTile(),new PlainTile(),new BlockingTile(),new PlainTile(),new PlainTile(),new BlockingTile(),new PlainTile(),new PlainTile()};
+        boardState[3] = new Tile[]{new CaveTile(),new BushTile(),new BlockingTile(),new BushTile(),new KoulouTile(),new BlockingTile(),new KoulouTile(),new PlainTile()};
+        boardState[4] = new Tile[]{new PlainTile(),new PlainTile(),new BlockingTile(),new BushTile(),new PlainTile(),new BlockingTile(),new PlainTile(),new BushTile()};
+        boardState[5] = new Tile[]{new KoulouTile(),new KoulouTile(),new BlockingTile(),new KoulouTile(),new PlainTile(),new BlockingTile(),new PlainTile(),new PlainTile()};
+        boardState[6] = new Tile[]{new PlainTile(),new PlainTile(),new BlockingTile(),new PlainTile(),new PlainTile(),new BlockingTile(),new PlainTile(),new PlainTile()};
+        boardState[7] = new Tile[]{new Market(items),new Market(items),new BlockingTile(),new Market(items),new Market(items),new BlockingTile(),new Market(items),new Market(items)};
 
+    }
     public void teleport(int heroID) {
         boolean accepted = false;
         int col;
---
+++ PlainTile.java
@@ -0,0 +1,11 @@
+public class PlainTile extends BoostTile{
+    @Override
+    public void addBoost(Hero h) {
+
+    }
+
+    @Override
+    public void removeBoost(Hero h) {
+
+    }
+}
