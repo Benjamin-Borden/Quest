f6b0bdbe6ba4f32b01ff9d151c470a600f658384
Elyse Kaczmarek <elysekaczmarek@gmail.com>
2020-04-14 10:25:18 -0700

    went back to a previous commit -- bug fix failed

--- LegendsGame.java
+++ LegendsGame.java
@@ -22,7 +22,7 @@ public class LegendsGame extends Game<LegendsBoard> {
         players[0].setHeroTurn(2);
         board.back(players[0].getHeroTurn());
         System.out.println(board);
-
+        
         System.out.println("Use wasd to move around! the i key will open your inventory and e will let you drink a potion or equip a weapon or armor!");
         System.out.println("Red I's are unpassable, Green triangles are common tiles where you might meet monsters, and blue M's are markets where you can buy things");
         System.out.println("Your heroes are the gold H!");
@@ -39,15 +39,22 @@ public class LegendsGame extends Game<LegendsBoard> {
                 players[0].setHeroTurn(i);
                 System.out.println(board);
 
-                char input = getHeroAction(i);
-                // don't consume an action when player asks for input
-                while(Character.toUpperCase(input)=='I'){
-                    players[0].displayParty();
-                    input = getHeroAction(i);
+                System.out.println("What would " + players[0].getParty()[i].getName() + " like to do?");
+                System.out.println("(WASD for movement, E for interacting with inventory, I for hero information, T to teleport to a different lane, or B to return back to the nexus.)");
+                boolean monsterCloseBy = board.monsterCloseBy(i);
+                char input;
+                if(monsterCloseBy){
+                    System.out.println("There's a monster close to you! You can attack it!\nYou can enter 1 for a regular attack, or 2 to cast a spell!");
+                    input = Input.getChar(new char[]{'W','w','A','a','S','s','D','d','I','i','Q','q','e','E', 't', 'T', 'b', 'B','1','2'});
+                }else{
+                    input = Input.getChar(new char[]{'W','w','A','a','S','s','D','d','I','i','Q','q','e','E', 't', 'T', 'b', 'B'});
                 }
+
                 if(Character.toUpperCase(input)=='Q'){
                     continuePlaying = false;
                     break;
+                }else if(Character.toUpperCase(input)=='I'){
+                    players[0].displayParty();
                 }else if(Character.toUpperCase(input)=='W'){
                     board.move(-1,0,i);
                 }else if(Character.toUpperCase(input)=='A'){
@@ -78,19 +85,6 @@ public class LegendsGame extends Game<LegendsBoard> {
         }while(continuePlaying);
     }
 
-    private char getHeroAction(int i) {
-        System.out.println("What would " + players[0].getParty()[i].getName() + " like to do?");
-        System.out.println("(WASD for movement, E for interacting with inventory, I for hero information, T to teleport to a different lane, or B to return back to the nexus.)");
-        boolean monsterCloseBy = board.monsterCloseBy(i);
-        char input;
-        if(monsterCloseBy){
-            System.out.println("There's a monster close to you! You can attack it!\nYou can enter 1 for a regular attack, or 2 to cast a spell!");
-            input = Input.getChar(new char[]{'W','w','A','a','S','s','D','d','I','i','Q','q','e','E', 't', 'T', 'b', 'B','1','2'});
-        }else{
-            input = Input.getChar(new char[]{'W','w','A','a','S','s','D','d','I','i','Q','q','e','E', 't', 'T', 'b', 'B'});
-        }
-        return input;
-    }
 
     // overload method
     public void equipOrDrink(int hero){
