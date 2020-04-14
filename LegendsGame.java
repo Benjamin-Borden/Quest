498dbab03144f1f2f27ae07714ba895de6db102c
Elyse Kaczmarek <elysekaczmarek@gmail.com>
2020-04-14 13:22:38 -0700

    testing wait

--- LegendsGame.java
+++ LegendsGame.java
@@ -38,12 +38,6 @@ public class LegendsGame extends Game<LegendsBoard> {
             // iterate through each hero in party for actions
             for(int i=0; i<players[0].getParty().length; i++) {
                 players[0].setHeroTurn(i);
-                try {
-                    Thread.sleep(1000);
-                }
-                catch(InterruptedException ex) {
-                        Thread.currentThread().interrupt();
-                }
                 System.out.println(board);
                 boolean validAction = false;
                 System.out.println("What would " + players[0].getParty()[i].getName() + " like to do?");
